5d8311541e0777b584f53ad44ea8a22d528c5c77
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-20 00:13:37 +0530

    add dark theme

---
+++ react_module01/tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+    darkMode: 'class', // default in v4, but can be explicit
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
